862d572e7c97d3d3b474d3c67aa1f48b7cb9d51c
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2024-09-09 20:54:21 -0300

    Aula04 - Intervalo

--- Aula03/Codigos/Moto.py
+++ Aula03/Codigos/Moto.py
@@ -4,7 +4,14 @@ class Moto(Veiculo):
     def __init__(self, marca, modelo, placa, ano, cilindradas):
         super().__init__(marca, modelo, placa,ano)
         self.__cilindradas = cilindradas
+    #Sobrescrita do método __str__()
+    def __str__(self):
+        #Invoco o método __str__() da SUPERCLASSE (Veiculo)
+        # Armazeno o retorno na variável "retorno"
+        retorno = super().__str__()
+        #Retorno a concatenação do valor da variável
+        # "retorno" com as "__cilindradas"
+        return f'''{retorno}
+ - Cilindradas: {self.__cilindradas}'''
 
-moto = Moto("Honda", "Falcon NX4", "abc", 2005, 400)
 
-print(moto)
