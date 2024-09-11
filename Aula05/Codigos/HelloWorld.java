4a094cd951823260a0d83282ba6caff62ec907bf
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2024-09-11 14:42:05 -0300

    Aula 05 e Aula 06

---
+++ Aula05/Codigos/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+    public int calcula(){
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int var1;
+        var1 = 2;
+
+        System.out.println(var1);
+
+    }
+}
