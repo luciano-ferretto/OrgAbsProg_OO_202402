018bd035c7aecfec50b3bfd31c6a11ded09dc36f
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2024-09-16 16:17:54 -0300

    Aula07 - Materiais PDFs

--- Aula05/Codigos/HelloWorld.java
+++ Aula05/Codigos/HelloWorld.java
@@ -1,14 +1,8 @@
 
 public class HelloWorld {
-    public int calcula(){
-        return 0;
-    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         int var1;
-        var1 = 2;
-
-        System.out.println(var1);
-
+        int var2 = 3;
+        System.out.println("Hello World!");
     }
 }
