91916779140abd848cd01694fcb131c2c5715166
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2024-09-18 19:40:09 -0300

    Calculadora IMC

--- Aula09/Códigos/ExemploWhile.java
+++ Aula09/Códigos/ExemploWhile.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class ExemploWhile {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        /*Neste exemplo a execução permanece em loop enquanto a variável opcao contiver o valor 'S' */
+        /*Neste exemplo a execução permanece em loop enquanto 
+              a variável opcao contiver o valor 'S' */
         char opcao = 'S';
         while (opcao == 'S') {
             System.out.println("Digite 'S' se deseja permanecer aqui?");
@@ -11,3 +12,5 @@ public class ExemploWhile {
         scan.close();
     }
 }
+
+
