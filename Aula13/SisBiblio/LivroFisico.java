b20a6c0e8defb1ffc20b8f991fbe94cf74dd7093
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2024-09-30 20:55:02 -0300

    Aula13 - Intervalo

---
+++ Aula13/SisBiblio/LivroFisico.java
@@ -0,0 +1,18 @@
+public class LivroFisico extends Livro {
+    private int nExemplares;
+    private String dimensoes;
+    public int getnExemplares() {
+        return nExemplares;
+    }
+    public void setnExemplares(int nExemplares) {
+        this.nExemplares = nExemplares;
+    }
+    public String getDimensoes() {
+        return dimensoes;
+    }
+    public void setDimensoes(String dimensoes) {
+        this.dimensoes = dimensoes;
+    }
+    
+    
+}
--- Aula13/SisBiblio/Main.java
+++ Aula13/SisBiblio/Main.java
@@ -39,10 +39,8 @@ public class Main {
 
         System.out.println("======== LISTA DE LIVROS =========");
         for (Livro livro : livros) {
-            System.out.println("Título: " + livro.getTitulo());
-            System.out.println("Autor: " + livro.getAutor());
-            System.out.println("Ano: " + livro.getAnoPublicacao());
-            System.out.println("N. Páginas: " + livro.getnPaginas());
+            System.out.println(livro);
+            //System.out.println(livro.toString());
             System.out.println("------------------------------------");
         }
         aguardarEnter();
@@ -50,8 +48,16 @@ public class Main {
 
     private static void adicionar() {
         limparTela();
-        Livro novoLivro = new Livro();
+        Livro novoLivro;
         System.out.println("======== ADICIONANDO NOVO LIVRO ========");
+        int tipoLivro = inputNumerico("Qual o tipo de livro: (1) Físico - (2) Digital");
+        if (tipoLivro == 1) {
+            novoLivro = new LivroFisico();
+        } else if (tipoLivro == 2) {
+            novoLivro = new LivroDigital();
+        } else {
+            novoLivro = new Livro();
+        }
         System.out.print("Informe o título do livro: ");
         String titulo = input.nextLine();
         novoLivro.setTitulo(titulo);
@@ -64,6 +70,18 @@ public class Main {
 
         int nPaginas = inputNumerico("Informe o número de páginas: ");
         novoLivro.setnPaginas(nPaginas);
+
+        if (tipoLivro == 1) {
+            int nExemplares = inputNumerico("Informe o nº de exemplares: ");
+            ((LivroFisico) novoLivro).setnExemplares(nExemplares);
+            System.out.println("Digite as dimensões: ");
+            String dimensoes = input.nextLine();
+            ((LivroFisico) novoLivro).setDimensoes(dimensoes);
+        } else if (tipoLivro == 2){
+            System.out.println("Digite o formato do arquivo: ");
+            String formatoArquivo = input.nextLine();
+            ((LivroDigital) novoLivro).setFormatoArquivo(formatoArquivo);
+        }
 
         try {
             biblio.adicionar(novoLivro);
