9c7a3d1f7368ab344048cfc8648500c9e0f33f65
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2024-11-11 16:23:11 -0300

    Material aula e API Sample

--- Aula16/html/script.js
+++ Aula16/html/script.js
@@ -14,21 +14,6 @@ document.getElementById("fetchButton").addEventListener("click", () => {
             document.getElementById("result").innerHTML = "Ocorreu um erro ao buscar os dados.";
             console.error("Erro:", error);
         });
-    fetch("https://swapi.dev/api/people/5")
-        .then(response => response.json())
-        .then(data => {
-            // Seleciona o elemento de resultado e insere o nome da resposta JSON
-            document.getElementById("result").innerHTML = `
-                <strong>Nome:</strong> ${data.name}<br>
-                <strong>Altura:</strong> ${data.height} cm<br>
-                <strong>Cor dos olhos:</strong> ${data.eye_color}<br>
-                <strong>Ano de nascimento:</strong> ${data.birth_year}
-            `;
-        })
-        .catch(error => {
-            document.getElementById("result").innerHTML = "Ocorreu um erro ao buscar os dados.";
-            console.error("Erro:", error);
-        });
 });
 document.getElementById("postButton").addEventListener("click", () => {
 
