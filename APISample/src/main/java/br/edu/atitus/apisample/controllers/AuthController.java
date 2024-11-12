f3f5180284bef694f9181da1ec52a0e009dca4d0
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2024-11-12 20:23:06 -0300

    ..

--- APISample/src/main/java/br/edu/atitus/apisample/controllers/AuthController.java
+++ APISample/src/main/java/br/edu/atitus/apisample/controllers/AuthController.java
@@ -2,6 +2,7 @@ package br.edu.atitus.apisample.controllers;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,22 +11,39 @@ import org.springframework.web.bind.annotation.RestController;
 import br.edu.atitus.apisample.dtos.SignupDTO;
 import br.edu.atitus.apisample.entities.TypeUser;
 import br.edu.atitus.apisample.entities.UserEntity;
+import br.edu.atitus.apisample.services.UserService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 	
+	// A classe AuthController possui uma dependência de "UserService"
+	private final UserService service;
+	// Injeção de dependência via método construtor
+	public AuthController(UserService service) {
+		super();
+		this.service = service;
+	}
+
 	@PostMapping("/signup")
 	public ResponseEntity<UserEntity> createNewUser(
-			@RequestBody SignupDTO signup){
+			@RequestBody SignupDTO signup) throws Exception{
 		// converter DTO2Entity
 		UserEntity newUser = new UserEntity();
 		BeanUtils.copyProperties(signup, newUser);
 		newUser.setType(TypeUser.Common);
 		
-		// TODO invocar método camada service
+		// invocar método camada service
+		service.save(newUser);
+		
 		// TODO retornar entidade User
 		return ResponseEntity.ok(newUser);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handlerMethod(Exception ex){
+		String msg = ex.getMessage().replaceAll("\r\n", "");
+		return ResponseEntity.badRequest().body(msg);
 	}
 
 }
--- APISample/src/main/java/br/edu/atitus/apisample/services/UserService.java
+++ APISample/src/main/java/br/edu/atitus/apisample/services/UserService.java
@@ -1,11 +1,20 @@
 package br.edu.atitus.apisample.services;
 
+import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Service;
 
 import br.edu.atitus.apisample.entities.UserEntity;
 
-@Service
+//Spring, gerencia objetos dessa classe pra mim
+@Service 
 public class UserService {
+	private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    public static boolean isValidEmail(String email) {
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
 	
 	public UserEntity save(UserEntity newUser) throws Exception {
 		// TODO Validar regras de negócio
@@ -19,9 +28,10 @@ public class UserService {
 		if (newUser.getEmail() == null || newUser.getEmail().isEmpty())
 			throw new Exception("Email inválido!");
 		//Validar o email com regex
+		if (!isValidEmail(newUser.getEmail()))
+			throw new Exception("Email inválido!");
 		
 		newUser.setEmail(newUser.getEmail().trim());
-		
 		
 		// TODO Invocar método camada repository
 		return newUser;
