98be32e907ffb6907e380b8ab193f9f0fb7ae009
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2024-11-13 19:13:34 -0300

    Final da aula 12/11

--- APISample/src/main/java/br/edu/atitus/apisample/entities/UserEntity.java
+++ APISample/src/main/java/br/edu/atitus/apisample/entities/UserEntity.java
@@ -4,8 +4,18 @@ import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_tb")
 public class UserEntity {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
 	private String name;
 	private String email;
